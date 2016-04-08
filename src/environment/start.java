5733f1064c617ae9f9186a6cf8a00547d224e660
Noah <nfd8@txstate.edu>
2016-04-08 15:03:55 -0500

    Added files via upload

--- src/environment/start.java
+++ src/environment/start.java
@@ -11,15 +11,27 @@ public class start {
 	    		+ "For an old school Command Line Interface, press 1\n"
 	    		+ "For a Graphical Interface, press 2\n");
 	    interfaceChoice = in.nextInt();
-	    //place holder, will beed to pass this to a function or class that switches interface
+	    //place holder, will need to pass this to a function or class that switches interface
 	    System.out.println(interfaceChoice);
 	}
 	
-	public static void NewGameQ(Scanner in){
+	public static boolean NewGameQ(Scanner in){
 		System.out.println("(N)ew Game\n");
 		System.out.println("(L)oad Game");
+		do{
+			NewGameChoice = in.next();
+			if(NewGameChoice.equals("n") || NewGameChoice.equals("N") || NewGameChoice.equals("new"))
+			{
+				return true;
+			}
+			else if(NewGameChoice.equals("l") || NewGameChoice.equals("L") || NewGameChoice.equals("load"))
+				return false;
+			else
+			{
+				System.out.println("You made an invalid selection, please enter either N for a new game, or L to load a saved game!");
+			}
+		}while(true);
 		
-		NewGameChoice = in.next();
 	}
 
 }
