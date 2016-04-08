817ee19fbef81f450c3c0437a8ee4e1bb710c401
Demi Sommerville <dsommerville13@gmail.com>
2016-04-08 11:21:07 -0500

    Added GUI code

--- src/mainDriver/mainDriver.java
+++ src/mainDriver/mainDriver.java
@@ -1,6 +1,14 @@
 package mainDriver;
 
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.Insets;
 import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 import charGen.charGen;
 import charGen.pointAllocator;
@@ -16,9 +24,53 @@ public class mainDriver {
 		//Allows user to choose between a CLI or a GUI
 		start startObj = new start();
 	    startObj.interfaceChoice(input);
-	    if(startObj.interfaceChoice == 2)
-	    	//yourGuiCodeHere
-	    	System.out.println(startObj.interfaceChoice);
+	    if(startObj.interfaceChoice == 2){
+	        JFrame frame1;
+	        Container pane;
+	        JButton btnNewGame, btnExit, btnLoadGame;
+	        JLabel lblGameName;
+	        JTextField txtServer;
+	        Insets insets;
+	    	
+	        //Create the frame
+	        frame1 = new JFrame ("Game GUI Test");
+	        //Set its size to 800x800 pixels
+	        frame1.setSize (800,800);
+	        //Prepare panel
+	        pane = frame1.getContentPane();
+	        insets = pane.getInsets();
+	        //Apply the null layout
+	        pane.setLayout (null);
+	        
+	        btnNewGame = new JButton ("New Game");
+	        btnLoadGame = new JButton ("Load Game");
+	        btnExit = new JButton ("Exit");
+	        lblGameName = new JLabel ("Le Creative Game Name");
+	        //txtServer = new JTextField (10);
+	        
+	        //set font
+	        lblGameName.setFont(new Font("Century Gothic", Font.PLAIN, 30));
+	        
+	        pane.add (lblGameName); //Add component to panel
+	        lblGameName.setBounds (215, 275, 400, 35);
+
+	        //Add all components to panel
+	        pane.add (btnNewGame);
+			pane.add (btnLoadGame);
+	        pane.add (btnExit);
+	 
+	        //Place all components  
+	/*      lblServer.setBounds (insets.left + 5, insets.top + 5, lblServer.getPreferredSize().width, lblServer.getPreferredSize().height);
+	        txtServer.setBounds (lblServer.getX() + lblServer.getWidth() + 5, insets.top + 5, txtServer.getPreferredSize().width, txtServer.getPreferredSize().height);*/
+	        btnNewGame.setBounds (350, 350, 100, 30);
+	        btnLoadGame.setBounds (350, 400, 100, 30);
+	        btnExit.setBounds (350, 450, 100, 30);
+
+	        frame1.setVisible (true);
+
+	    }
+	    else{
+	    	
 	    //Star a NewGame or LoadGame
 	    start.NewGameQ(input);
 		//Create a player object
@@ -42,7 +94,7 @@ public class mainDriver {
 		//Function can be used, when called to do so, to allow player to see their
 		//current stats during play period
 		playerCharacter.printStats();
-		
+	    }
 		input.close();
 	}
 }
