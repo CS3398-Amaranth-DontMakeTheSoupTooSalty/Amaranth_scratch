19d3121b05c6cc73a5a3f0f40f4d4e90ee56f5da
Christina <Christina@10.36.137.78>
2016-04-12 03:34:13 -0500

    remove of the double gui popup

--- src/mainDriver/mainDriver.java
+++ src/mainDriver/mainDriver.java
@@ -39,48 +39,7 @@ public class mainDriver {
 	    	objGui.launchGUI();
 	    	
 	    	   
-	        JFrame frame1;
-	        Container pane;
-	        JButton btnNewGame, btnExit, btnLoadGame;
-	        JLabel lblGameName;
-	        //JTextField txtServer;
-	        //Insets insets;
-	    	
-	        //Create the frame
-	        frame1 = new JFrame ("Game GUI Test");
-	        //Set its size to 800x800 pixels
-	        frame1.setSize (800,800);
-	        //Prepare panel
-	        pane = frame1.getContentPane();
-	        //insets = pane.getInsets();
-	        //Apply the null layout
-	        pane.setLayout (null);
-	        
-	        btnNewGame = new JButton ("New Game");
-	        btnLoadGame = new JButton ("Load Game");
-	        btnExit = new JButton ("Exit");
-	        lblGameName = new JLabel ("Le Creative Game Name");
-	        //txtServer = new JTextField (10);
-	        
-	        //set font
-	        lblGameName.setFont(new Font("Century Gothic", Font.PLAIN, 30));
-	        
-	        pane.add (lblGameName); //Add component to panel
-	        lblGameName.setBounds (215, 275, 400, 35);
-
-	        //Add all components to panel
-	        pane.add (btnNewGame);
-			pane.add (btnLoadGame);
-	        pane.add (btnExit);
-	 
-	        //Place all components  
-	/*      lblServer.setBounds (insets.left + 5, insets.top + 5, lblServer.getPreferredSize().width, lblServer.getPreferredSize().height);
-	        txtServer.setBounds (lblServer.getX() + lblServer.getWidth() + 5, insets.top + 5, txtServer.getPreferredSize().width, txtServer.getPreferredSize().height);*/
-	        btnNewGame.setBounds (350, 350, 100, 30);
-	        btnLoadGame.setBounds (350, 400, 100, 30);
-	        btnExit.setBounds (350, 450, 100, 30);
-
-	        frame1.setVisible (true);
+	       
 	    }
 	    else if(startObj.interfaceChoice == 1){
 	    	
