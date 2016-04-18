455707a2d476aadb5829d81df20ec645f74844b4
Justin Jordan <jordan.justinb@gmail.com>
2016-04-18 11:02:38 -0500

    Preliminary sprite animation package

---
+++ src/spriteAnimation/frame.java
@@ -0,0 +1,31 @@
+package spriteAnimation;
+
+import java.awt.image.BufferedImage;
+
+public class frame {
+
+    private BufferedImage frame;
+    private int duration;
+
+    public frame(BufferedImage frame, int duration) {
+        this.frame = frame;
+        this.duration = duration;
+    }
+
+    public BufferedImage getFrame() {
+        return frame;
+    }
+
+    public void setFrame(BufferedImage frame) {
+        this.frame = frame;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+}
